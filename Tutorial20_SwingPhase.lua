11b4edbd449f01d2adb296107ee7aca9eab36169
青山宏樹 <aoyamahiroki@MacBook-Pro-2.local>
2026-02-28 20:21:25 +0900

    Update swingDuration to 0.7s based on physical simulation time

--- Tutorial20_SwingPhase.lua
+++ Tutorial20_SwingPhase.lua
@@ -42,9 +42,9 @@ function sysCall_init()
     sim.setJointTargetPosition(rHipJointHandle, -20 * math.pi / 180)
     -- 脚全体の重さに負けて揺れないよう、モーターの出す最大力を超強力（1000）にセット
     sim.setJointTargetForce(rHipJointHandle, 1000)
-    
+    -- 2. 初期姿勢（Toe-off）を維持するための保持トルク
     -- 遊脚期の時間
-    swingDuration = 0.5
+    swingDuration = 0.7
     
     -- [Hip] 太もも（RThigh）を回転させる（持ち上げる）ためのトルク
     -- 遊脚相全体（swingDuration）のうち、前方向に振り上げ続ける時間の割合（0.0 〜 1.0）
